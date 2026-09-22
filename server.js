7cfca8209edffa5d2c8c4ec519071390666767b0
nsir2500-oss <nsir2500@gmail.com>
2026-09-22 18:43:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ const OpenAI = require("openai");
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
 
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
